2b779de29a4446a7b6a62f7af596d9e34dd80937
Chris Hartley <cahartley@gmail.com>
2017-04-15 18:21:01 -0700

    Production settings

--- evernotetodos_dj/evernotetodos_dj/settings.py
+++ evernotetodos_dj/evernotetodos_dj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'url_3h_lb3i31wcuu+7a5)&n6$mdhx4z6-&q7mi0sifxxgkx-9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/'),
 
 # As per http://django-allauth.readthedocs.io/en/latest/installation.html
 
@@ -138,7 +139,7 @@ AUTHENTICATION_BACKENDS = (
 
 # Site required; http://stackoverflow.com/a/35389220
 
-SITE_ID = 1
+SITE_ID = 3
 
 # django-allauth conf
 
