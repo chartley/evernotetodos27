6cf83981ce4463dcec32e5d36ae8e1c3e806c369
Chris Hartley <cahartley@gmail.com>
2017-10-14 22:12:55 -0700

    Check for single access token

--- evernotetodos_dj/evernotetodos/utils.py
+++ evernotetodos_dj/evernotetodos/utils.py
@@ -10,10 +10,10 @@ def get_user_evernote_note(user):
         raise Exception('>1 Evernote account for %s ' % user.pk)
     account = evernote_accts[0]
 
-    if not account.socialtoken_set.exists():
-        raise Exception('No SocialTokens linked to %s' % user.pk)
-
+    if account.socialtoken_set.count() != 1:
+        raise Exception('SocialTokens %d when 1 expected for user %s' % (account.socialtoken_set.count(), user.pk))
     socialtoken = account.socialtoken_set.all()[0]
+
     print 'get_user_evernote_note(%s): token: %s' % (user.pk, socialtoken.token)
 
     return socialtoken.token
